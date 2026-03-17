739609b38b6e1ea579f4806e31a65c583f3e30aa
Evan <68429595+ConsciousBone@users.noreply.github.com>
2026-03-17 13:28:14 +0000

    Initial Commit

---
+++ Equator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Equator
+//
+//  Created by Evan Plant on 17/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Equator/EquatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  EquatorApp.swift
+//  Equator
+//
+//  Created by Evan Plant on 17/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EquatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
